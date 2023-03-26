1adb9f14b2ef2f7dc422018e7966d9057e471d9e
Alexander Ershov <a.ershov@lancktele.com>
2023-03-26 08:55:08 +0000

    refactor

--- src/main/java/com/example/cloudStorage/handler/HandlerController.java
+++ src/main/java/com/example/cloudStorage/handler/HandlerController.java
@@ -6,6 +6,7 @@ import com.example.cloudStorage.exceptions.FilenameError;
 import com.example.cloudStorage.models.ErrorResponse;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.InternalAuthenticationServiceException;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -18,7 +19,8 @@ public class HandlerController {
         return new ResponseEntity<>(new ErrorResponse(exception.getMessage(), 1), HttpStatus.BAD_REQUEST);
     }
 
-    @ExceptionHandler({BadCredentials.class, TokenExpiredException.class, UsernameNotFoundException.class})
+    @ExceptionHandler({BadCredentials.class, TokenExpiredException.class, UsernameNotFoundException.class,
+            InternalAuthenticationServiceException.class})
     public ResponseEntity<ErrorResponse> credentialsError(Exception exception) {
         return new ResponseEntity<>(new ErrorResponse(exception.getMessage(), 0), HttpStatus.UNAUTHORIZED);
     }
