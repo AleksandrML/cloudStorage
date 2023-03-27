de321cda6e28f9258fd1cf07b93d9bdce6c39cb7
Alexander Ershov <a.ershov@lancktele.com>
2023-03-27 18:41:59 +0000

    move secret to envs

--- src/main/java/com/example/cloudStorage/security/SecurityConstants.java
+++ src/main/java/com/example/cloudStorage/security/SecurityConstants.java
@@ -1,10 +1,20 @@
 package com.example.cloudStorage.security;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Repository;
+
+@Repository
 public class SecurityConstants {
 
-    public static final String SECRET = "SECRET_KEY";
+    public static String SECRET;
     public static final long EXPIRATION_TIME = 900_000; // 15 mins
     public static final String TOKEN_PREFIX = "Bearer ";
     public static final String HEADER_STRING = "auth-token";
     public static final String SIGN_UP_URL = "/api/user";
+
+    @Value("${security.secret}")
+    public void setSecret(String secret) {
+        SECRET = secret;
+    }
+
 }
