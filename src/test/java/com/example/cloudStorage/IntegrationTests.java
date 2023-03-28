20139127271bf73fd84f3e6a8335e84323d6c268
aleksandr <ershets@gmail.com>
2023-03-29 00:15:54 +0300

    add integration tests finally

--- src/test/java/com/example/cloudStorage/IntegrationTests.java
+++ src/test/java/com/example/cloudStorage/IntegrationTests.java
@@ -1,36 +1,35 @@
 package com.example.cloudStorage;
 
+import com.example.cloudStorage.security.SecurityConstants;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
-import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.web.client.RestTemplate;
 import org.testcontainers.containers.GenericContainer;
 import org.testcontainers.shaded.com.fasterxml.jackson.core.JsonProcessingException;
 import org.testcontainers.shaded.com.fasterxml.jackson.databind.JsonNode;
 import org.testcontainers.shaded.com.fasterxml.jackson.databind.ObjectMapper;
-
 import java.util.Map;
 
-//@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @SpringBootTest(classes = TestConfig.class)
 class IntegrationTests {
-//	@Autowired
-//	TestRestTemplate restTemplate;
+
+	// because of usage mokDb connection from testConfig during running tests (real connection has docker container)
+	// I can not use autowiring of TestRestTemplate restTemplate with classical solution
+	// webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT, so I will use full version of RestTemplate
 
 	// one has to use .env config (or ci/cd configs) or set run configuration with envs
 	static String DB_URL = System.getenv("DB_URL");
 	static String DB_USERNAME = System.getenv("DB_USERNAME");
 	static String DB_PASSWORD = System.getenv("DB_PASSWORD");
 	static String SECRET = System.getenv("SECRET_KEY");
+    static String userTest = "USER_TESTttyyffddfff1";
+    static String passwordTest = "PASSWORD_TESTttyyfffddfff1";
 
 	static Map<String, String> envMap = Map.of(
 			"DB_URL", DB_URL,
@@ -41,55 +40,49 @@ class IntegrationTests {
 	// one has to have previously built docker image locally from the project and run pg db to run that test
 	private static final GenericContainer<?> container = new GenericContainer<>("cloud-storage:latest")
 			.withExposedPorts(8081).withEnv(envMap);
-	private final ObjectMapper objectMapper = new ObjectMapper();
+	private static final ObjectMapper objectMapper = new ObjectMapper();
 
 	@BeforeAll
 	public static void setUp() {
+		System.out.println(SECRET);
 		container.start();
+        // create test user
+		// given:
+		RestTemplate restTemplate = new RestTemplate();
+		HttpHeaders headers = new HttpHeaders();
+		headers.setContentType(MediaType.APPLICATION_JSON);
+		HttpEntity<Map<String, Object>> entity = new HttpEntity<>(
+				Map.of("username", userTest, "password", passwordTest), headers);
+
+		// when:
+		ResponseEntity<String> response = restTemplate.postForEntity(
+				"http://localhost:" + container.getMappedPort(8081) + SecurityConstants.SIGN_UP_URL,
+				entity, String.class);
+
+		// then:
+		Assertions.assertTrue(response.getStatusCode().is2xxSuccessful());
 	}
 
-//	@Test
-//	void testTransfer() throws JsonProcessingException {
-//		//given
-//		String requestJson = "{\"cardFromNumber\": \"not_valid\", \"cardFromValidTill\": \"12/25\"," +
-//				" \"cardFromCVV\": \"330\", \"cardToNumber\": \"not_valid\"," +
-//				"\"amount\": {\"value\": 1000, \"currency\": \"RUR\"}}";
-//		HttpHeaders headers = new HttpHeaders();
-//		headers.setContentType(MediaType.APPLICATION_JSON);
-//		HttpEntity<String> entity = new HttpEntity<>(requestJson, headers);
-//
-//		//expected
-//		String expectedMessage = "\"Acceptor card is not acceptable for transaction\"";
-//
-//		//when
-//		ResponseEntity<String> response =
-//				restTemplate.postForEntity("http://localhost:" + container.getMappedPort(5500) + "/transfer", entity, String.class);
-//		JsonNode root = objectMapper.readTree(response.getBody());
-//		String realMessage = root.path("message").toString();
-//
-//		//then
-//		Assertions.assertEquals(expectedMessage, realMessage);
-//	}
-//
-//	@Test
-//	void testConfirm() throws JsonProcessingException {
-//		//given
-//		String requestJson = "{\"operationId\": \"no_existed_id\", \"code\": \"wrong_code\"}";
-//		HttpHeaders headers = new HttpHeaders();
-//		headers.setContentType(MediaType.APPLICATION_JSON);
-//		HttpEntity<String> entity = new HttpEntity<>(requestJson, headers);
-//
-//		//expected
-//		String expectedMessage = "\"there is no such transaction or code is wrong\"";
-//
-//		//when
-//		ResponseEntity<String> response =
-//				restTemplate.postForEntity("http://localhost:" + container.getMappedPort(5500) + "/confirmOperation", entity, String.class);
-//		JsonNode root = objectMapper.readTree(response.getBody());
-//		String realMessage = root.path("message").toString();
-//
-//		//then
-//		Assertions.assertEquals(expectedMessage, realMessage);
-//	}
+    @Test
+	void testLogin() throws JsonProcessingException {
+		// given:
+		RestTemplate restTemplate = new RestTemplate();
+		HttpHeaders headers = new HttpHeaders();
+		headers.setContentType(MediaType.APPLICATION_JSON);
+		HttpEntity<Map<String, Object>> entity = new HttpEntity<>(
+				Map.of("login", userTest, "password", passwordTest), headers);
+
+		// when:
+		ResponseEntity<String> response = restTemplate.postForEntity(
+				"http://localhost:" + container.getMappedPort(8081) + "/login",
+				entity, String.class);
+		JsonNode root = objectMapper.readTree(response.getBody());
+		String token = root.path("auth-token").toString();
+
+		// then:
+		Assertions.assertTrue(response.getStatusCode().is2xxSuccessful());
+		// check if token is formed as expected:
+		Assertions.assertTrue(token.startsWith("\"Bearer"));
+    }
 
 }
